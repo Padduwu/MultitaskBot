e633ae0d1eafdecdeff475097743620a6e995106
Padduwu <52707511+Padduwu@users.noreply.github.com>
2020-03-11 22:35:20 +0000

    add gui initial

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from firefly import FireflyBot
+from tkinter import *
 
-def setup():
+def run_firefly():
     bot = FireflyBot()
     bot.take_instructions()
 
-setup()
+def create_window():
+    root = Tk()
+    button = Button(root, text="Click me", padx=50)
+    button.pack()
+    root.mainloop()
+create_window()
+
+
+
