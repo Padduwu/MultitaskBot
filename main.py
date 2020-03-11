4a2f68787ea3c5699dd694b81b93dba3e8d85e87
Padduwu <52707511+Padduwu@users.noreply.github.com>
2020-03-11 23:00:30 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ def run_firefly():
     bot = FireflyBot()
     bot.take_instructions()
 
+run_firefly()
 def create_window():
     root = Tk()
     button = Button(root, text="Click me", padx=50)
     button.pack()
     root.mainloop()
-create_window()
+
 
 
 
